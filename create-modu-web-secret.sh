7cc20580b0a4a2521a38ae8dc6e58f0944505656
Junil Um <powerumc@gmail.com>
2018-05-07 22:42:40 +0900

    Create create-modu-web-secret.sh

---
+++ create-modu-web-secret.sh
@@ -0,0 +1,5 @@
+kubectl create secret docker-registry aws-ecr \
+    --docker-server=299277078007.dkr.ecr.ap-northeast-2.amazonaws.com \
+    --docker-username=AWS \
+    --docker-password=[password] \
+    --docker-email=none
